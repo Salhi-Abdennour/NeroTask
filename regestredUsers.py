500a322c2d65b23e60723b4fc2652c7742793b1a
CHR0N1CL3 <45244507+chrony-DevLogs@users.noreply.github.com>
2023-03-02 10:08:21 -0800

    Update regestredUsers.py

--- regestredUsers.py
+++ regestredUsers.py
@@ -12,21 +12,3 @@ class User:
         self.toDos = todos
     def getToDos(self):
         return self.toDos
-
-t = numpy.array([numpy.array([int]*5)]*9)
-
-def sim(m,x,y):
-    for i in range(0,y):
-        for j in range(0,x):
-            m[i,j] = 0
-    for i in range(0,y):
-        m[i,0] = 1
-
-    for i in range(round(y/2)+1,y):
-        for j in range(1,x):
-            m[i,j] = m[i-1,j-1] + m[i-1,j]
-
-    for i in range(0,y):
-        t[i] = t[y-1-i]
-sim(t,5,9)
-print(t)
